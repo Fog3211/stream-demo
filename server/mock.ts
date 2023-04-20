1ec4b6df33cb939dbac5528ad4dc916d11c9edc7
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2023-04-20 11:13:27 +0800

    chore: update npm script

--- server/mock.ts
+++ server/mock.ts
@@ -1,6 +1,10 @@
 import fs from 'node:fs';
 import path from 'node:path';
 import EventEmitter from 'events';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const generateFileData = () => {
   const filePath = path.join(__dirname, './article.txt');
